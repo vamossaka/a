4b7a8ea5b3557f6b4cc09e201e20711fb47a5f63
yinqiao wang <2181538575@qq.com>
2023-05-23 10:23:24 +0800

    third

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,7 +1,7 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class LinkedListDeque<T> {
-    public class node{
+    private class node{
         private T item;
         private node pre;
         private node next;
